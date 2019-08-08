411bbdd172afc942b04c468a26527939531a4735
Morten Elbo <mortenelbo01@gmail.com>
2019-08-08 16:36:16 +0100

    logic functions added

--- JS/main.js
+++ JS/main.js
@@ -3,21 +3,28 @@ var cards = ["queen","queen","king","king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
 
-cardsInPlay.push(cardOne); 
-cardsInPlay.push(cardTwo);
+function flipCard(cardId) {
 
-if (cardsInPlay.length === 2){
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
 
-	console.log("Cards played 2");
+
 }
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!") 
-} else {alert("No match. Try again!");}
+flipCard(0);
+flipCard(1);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2){
+	console.log("Cards played : 2");
+ 	checkForMatch();}
+
+
+function checkForMatch() {
+ 	
+ 	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");} 
+	else { console.log("No match. Try again!"); }
+}
+
 
