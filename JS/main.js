3d061cb62fb8e56fdd8a0a66a9d9bc86c4a14f8c
Morten Elbo <mortenelbo01@gmail.com>
2019-08-08 09:17:07 +0100

    game logic created

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,23 @@
 console.log("Up and running");
+var cards = ["queen","queen","king","king"];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne); 
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2){
+
+	console.log("Cards played 2");
+}
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!") 
+} else {alert("No match. Try again!");}
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
+
