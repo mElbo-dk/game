87e40a41dca7b561e01a5887c52ce47f944246a8
Morten Elbo <mortenelbo01@gmail.com>
2019-08-09 11:11:45 +0100

    logic functions added

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,39 @@
 console.log("Up and running");
-var cards = ["queen","queen","king","king"];
+var cards = [{
+	rank: "queen",
+	suit: "hearts",	
+	cardImage:"images/queen-of-hearts.png"
+},{
+	rank: "queen",
+	suit: "diamonds",	
+	cardImage:"images/queen-of-diamonds.png"
+},{
+	rank: "king",
+	suit: "hearts",	
+	cardImage:"images/king-of-hearts.png"
+
+},{
+	rank: "king",
+	suit: "diamonds",	
+	cardImage:"images/king-of-diamonds.png"
+}];
 
 var cardsInPlay = [];
 
 
 function flipCard(cardId) {
 
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log("User flipped " + cards[cardId].suit);
+console.log("User flipped " + cards[cardId].cardImage);
+
+cardsInPlay.push(cards[cardId].rank);
 
 
 }
 
-flipCard(0);
-flipCard(1);
+flipCard(3);
+flipCard(3);
 
 if (cardsInPlay.length === 2){
 	console.log("Cards played : 2");
