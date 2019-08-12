71a8bbe2d29a1c1ce530d40d8ee0e7dbd10fcd04
Morten Elbo <mortenelbo01@gmail.com>
2019-08-12 09:04:46 +0100

    JS logic functions added

--- JS/main.js
+++ JS/main.js
@@ -21,30 +21,55 @@ var cards = [{
 var cardsInPlay = [];
 
 
-function flipCard(cardId) {
-
+function flipCard() {
+var cardId = this.getAttribute("data-id");
+cardsInPlay.push(cards[cardId].rank);
 console.log("User flipped " + cards[cardId].rank);
 console.log("User flipped " + cards[cardId].suit);
 console.log("User flipped " + cards[cardId].cardImage);
-
-cardsInPlay.push(cards[cardId].rank);
-
-
+this.setAttribute("src" , cards[cardId].cardImage);
+setTimeout(checkFlip, 100);
 }
 
-flipCard(3);
-flipCard(3);
 
-if (cardsInPlay.length === 2){
-	console.log("Cards played : 2");
- 	checkForMatch();}
 
+
+
+function checkFlip() {
+	if (cardsInPlay.length == 2){
+		console.log("Cards played : 2");
+ 		checkForMatch();
+ 	}else{ return;}
+}
 
 function checkForMatch() {
- 	
  	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");} 
-	else { console.log("No match. Try again!"); }
+	alert("You found a match!");
+	clearBoard();
+
+} 
+	else { alert("No match. Try again!"); 
+	clearBoard();
+}
 }
 
+
+function clearBoard(){
+	cardsInPlay.pop();
+	cardsInPlay.pop();
+	createBoard();	}
+
+function createBoard(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+    	cardElement.setAttribute("src" , "images/back.png");
+    	cardElement.setAttribute("data-id" , i);
+    	cardElement.addEventListener("click" , flipCard);
+    	console.log(cardElement);    	
+    	document.getElementById("game-board").appendChild(cardElement);
+    	
+    }
+}
+
+createBoard();
 
